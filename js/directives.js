0ef6a03727b4a2a99fac6f106ce05b8eb61a6062
Daniel Henriksen <danhenriksen12@gmail.com>
2013-08-28 10:47:02 -0700

    Connect to firebase

---
+++ js/directives.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Directives */
+// Directives are for manipulating the DOM
+
+angular.module('myApp.directives', []).
+  directive('appVersion', ['version', function(version) {
+    return function(scope, elm, attrs) {
+      elm.text(version);
+    };
+  }]);
