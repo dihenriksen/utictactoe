3909c2e2d5bf5d788e6d44388049a26c6797e795
Daniel Henriksen <danhenriksen12@gmail.com>
2013-08-28 15:19:27 -0700

    X toggles on and off on click

--- js/controllers.js
+++ js/controllers.js
@@ -9,22 +9,20 @@ angular.module('utictactoe.controllers', []).
   	function($scope, angularFire) {
   		var url = 'http://utictactoe.firebaseio.com/gameboard';
   		var promise = angularFire(url, $scope, 'gameboard', {'spaces': []});
-      // console.log(gameboard);
-
-
-      // gameboard.spaces = [];
 
   		$scope.myFunc = function() {
   		}
 
-  		$scope.makeMove = function(i) {
-        console.log(i);
-        $scope.gameboard.spaces[i] = 'X';
-        console.log($scope.gameboard.spaces[0]);
-  		}
+  		$scope.spaceSelect = function(i) {
+        if ($scope.gameboard.spaces[i] === 'X' || $scope.gameboard.spaces[i] === 'O') {
+          $scope.gameboard.spaces[i] = '';
+        } else {
+          $scope.gameboard.spaces[i] = 'X';
+        };
+      };
 
   		promise.then(function() {
-  			$scope.$watch('board', $scope.myFunc);
+  			$scope.$watch('gameboard', $scope.myFunc);
         console.log($scope.gameboard);
   		});
   	}
