2f4a2776e6e8f574ba12bdeeabd30f6ee57ea6b8
Daniel Henriksen <danhenriksen12@gmail.com>
2013-08-30 15:25:38 -0700

    Only legal moves allowed. Turns alternate.

--- js/controllers.js
+++ js/controllers.js
@@ -9,22 +9,29 @@ angular.module('utictactoe.controllers', []).
   	function($scope, angularFire) {
   		var url = 'http://utictactoe.firebaseio.com/gameboard';
   		var promise = angularFire(url, $scope, 'gameboard', {'spaces': []});
+      var enabled = [];
+      var moves = [];
+      for (var i = 0; i <= 88; i++) {
+        enabled.push(i);
+      }
+      var turn = true;
 
   		$scope.myFunc = function() {
   		}
 
-      var turn = true;
-
   		$scope.spaceSelect = function(i) {
-        if ($scope.gameboard.spaces[i] === 'X' || $scope.gameboard.spaces[i] === 'O') {
-          $scope.gameboard.spaces[i] = '';
-        } else if (turn === true) {
-          $scope.gameboard.spaces[i] = 'X';
-        } else if (turn === false) {
-          $scope.gameboard.spaces[i] = 'O';
-        };
-        $scope.enableSectors(i);
-        turn = !turn;
+        if (enabled.indexOf(i) != -1) {
+          if (turn) {
+            $scope.gameboard.spaces[i] = 'X';
+          } else {
+            $scope.gameboard.spaces[i] = 'O';
+          }
+          moves.push(i);
+          enabled = [];
+          console.log(moves);
+          $scope.enableSectors(i);
+          turn = !turn;
+        }
       };
 
       $scope.enableSectors = function(i) {
@@ -39,6 +46,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 0; i <= 8; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 1: case 11: case 21: case 31: case 41: case 51: case 61: case 71: case 81:
             $scope.disabled0 = true;
@@ -50,6 +62,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 10; i <= 18; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 2: case 12: case 22: case 32: case 42: case 52: case 62: case 72: case 82:
             $scope.disabled0 = true;
@@ -61,6 +78,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 20; i <= 28; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 3: case 13: case 23: case 33: case 43: case 53: case 63: case 73: case 83:
             $scope.disabled0 = true;
@@ -72,6 +94,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 30; i <= 38; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 4: case 14: case 24: case 34: case 44: case 54: case 64: case 74: case 84:
             $scope.disabled0 = true;
@@ -83,6 +110,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 40; i <= 48; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 5: case 15: case 25: case 35: case 45: case 55: case 65: case 75: case 85:
             $scope.disabled0 = true;
@@ -94,6 +126,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 50; i <= 58; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 6: case 16: case 26: case 36: case 46: case 56: case 66: case 76: case 86:
             $scope.disabled0 = true;
@@ -105,6 +142,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = false;
             $scope.disabled7 = true;
             $scope.disabled8 = true;
+            for (var i = 60; i <= 68; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 7: case 17: case 27: case 37: case 47: case 57: case 67: case 77: case 87:
             $scope.disabled0 = true;
@@ -116,6 +158,11 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = false;
             $scope.disabled8 = true;
+            for (var i = 70; i <= 78; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
           case 8: case 18: case 28: case 38: case 48: case 58: case 68: case 78: case 88:
             $scope.disabled0 = true;
@@ -127,11 +174,21 @@ angular.module('utictactoe.controllers', []).
             $scope.disabled6 = true;
             $scope.disabled7 = true;
             $scope.disabled8 = false;
+            for (var i = 80; i <= 88; i++) {
+              if (moves.indexOf(i) == -1) {
+                enabled.push(i);
+              }
+            }
             break
         }
       }
 
       $scope.newgame = function() {
+        enabled = [];
+        for (var i = 0; i <= 88; i++) {
+          enabled.push(i);
+        }
+        moves = [];
         for (var space in $scope.gameboard.spaces) {
           $scope.gameboard.spaces[space] = '';
         }
@@ -145,7 +202,6 @@ angular.module('utictactoe.controllers', []).
         $scope.disabled7 = false;
         $scope.disabled8 = false;
         $scope.disabled9 = false;
-        console.log($scope.disabled0);
       }
 
   		promise.then(function() {
