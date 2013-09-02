34a7a5a1687161ddbed3f0fb8ae82be0836471d2
Daniel Henriksen <danhenriksen12@gmail.com>
2013-09-02 14:25:43 -0700

    Fix problem showing allowed spaces when sent to a filled sector

--- game/app/js/controllers.js
+++ game/app/js/controllers.js
@@ -8,14 +8,14 @@ angular.module('utictactoe.controllers', []).
   	'angularFire',
   	function($scope, angularFire) {
   		var url = 'http://utictactoe.firebaseio.com/gameboard';
-  		var promise = angularFire(url, $scope, 'gameboard', {'spaces': [], 'moves': []});
+  		var promise = angularFire(url, $scope, 'gameboard', {});
 
       // turn = true means it is X's turn
       // turn = false means it is O's turn
       var turn = true;
 
+      // This function makes a move on the board.
   		$scope.spaceSelect = function(i) {
-        console.log($scope.gameboard.enabled);
         if ($scope.gameboard.enabled.indexOf(i) != -1) {
           if (turn) {
             $scope.gameboard.spaces[i] = 'X';
@@ -24,16 +24,17 @@ angular.module('utictactoe.controllers', []).
           }
           $scope.gameboard.moves.push(i);
           $scope.gameboard.enabled = [];
-          $scope.setEnabled(i);
           $scope.showEnabledSectors(i);
+          $scope.setEnabled(i);
+          console.log($scope.disabledSects[0]);
           turn = !turn;
         }
       };
 
       //controls the squares that are enabled, sending data to firebase
       $scope.setEnabled = function(k) {
-        // i will in general be a square that is clicked.
-        // if it is to start a game, i should be set to -1 when calling setEnabled
+        // k will in general be a square that is clicked.
+        // if it is to start a game, k should be set to -1 when calling setEnabled
         switch (k) {
           // starting a new game:
           case -1:
@@ -97,7 +98,7 @@ angular.module('utictactoe.controllers', []).
             arr.push(i);
           }
         }
-        $scope.disabled0 = 'all';$scope.disabled1 = 'all';$scope.disabled2 = 'all';$scope.disabled3 = 'all';$scope.disabled4 = 'all';$scope.disabled5 = 'all';$scope.disabled6 = 'all';$scope.disabled7 = 'all';$scope.disabled8 = 'all';$scope.disabled9 = 'all';
+        $scope.disabledSects[0] = 'all';$scope.disabledSects[1] = 'all';$scope.disabledSects[2] = 'all';$scope.disabledSects[3] = 'all';$scope.disabledSects[4] = 'all';$scope.disabledSects[5] = 'all';$scope.disabledSects[6] = 'all';$scope.disabledSects[7] = 'all';$scope.disabledSects[8] = 'all';
         return arr;
       }
 
@@ -105,31 +106,31 @@ angular.module('utictactoe.controllers', []).
       $scope.showEnabledSectors = function(i) {
         switch (i) {
           case 0: case 10: case 20: case 30: case 40: case 50: case 60: case 70: case 80:
-            $scope.disabled0 = false;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = false;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 1: case 11: case 21: case 31: case 41: case 51: case 61: case 71: case 81:
-            $scope.disabled0 = true;$scope.disabled1 = false;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = false;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 2: case 12: case 22: case 32: case 42: case 52: case 62: case 72: case 82:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = false;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = false;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 3: case 13: case 23: case 33: case 43: case 53: case 63: case 73: case 83:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = false;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = false;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 4: case 14: case 24: case 34: case 44: case 54: case 64: case 74: case 84:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = false;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = false;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 5: case 15: case 25: case 35: case 45: case 55: case 65: case 75: case 85:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = false;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = false;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 6: case 16: case 26: case 36: case 46: case 56: case 66: case 76: case 86:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = false;$scope.disabled7 = true;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = false;$scope.disabledSects[7] = true;$scope.disabledSects[8] = true;
             break
           case 7: case 17: case 27: case 37: case 47: case 57: case 67: case 77: case 87:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = false;$scope.disabled8 = true;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = false;$scope.disabledSects[8] = true;
             break
           case 8: case 18: case 28: case 38: case 48: case 58: case 68: case 78: case 88:
-            $scope.disabled0 = true;$scope.disabled1 = true;$scope.disabled2 = true;$scope.disabled3 = true;$scope.disabled4 = true;$scope.disabled5 = true;$scope.disabled6 = true;$scope.disabled7 = true;$scope.disabled8 = false;
+            $scope.disabledSects[0] = true;$scope.disabledSects[1] = true;$scope.disabledSects[2] = true;$scope.disabledSects[3] = true;$scope.disabledSects[4] = true;$scope.disabledSects[5] = true;$scope.disabledSects[6] = true;$scope.disabledSects[7] = true;$scope.disabledSects[8] = false;
             break
         }
       }
@@ -144,9 +145,8 @@ angular.module('utictactoe.controllers', []).
         }
         $scope.setEnabled(-1);
 
-        $scope.allEnabled = true;
-
-        $scope.disabled0 = 'all';$scope.disabled1 = 'all';$scope.disabled2 = 'all';$scope.disabled3 = 'all';$scope.disabled4 = 'all';$scope.disabled5 = 'all';$scope.disabled6 = 'all';$scope.disabled7 = 'all';$scope.disabled8 = 'all';$scope.disabled9 = 'all';
+        $scope.disabledSects = [];
+        $scope.disabledSects[0] = 'all';$scope.disabledSects[1] = 'all';$scope.disabledSects[2] = 'all';$scope.disabledSects[3] = 'all';$scope.disabledSects[4] = 'all';$scope.disabledSects[5] = 'all';$scope.disabledSects[6] = 'all';$scope.disabledSects[7] = 'all';$scope.disabledSects[8] = 'all';
       }
 
   		promise.then(function() {
